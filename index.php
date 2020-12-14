fe072f852b15a157697c352742c76722ed89bdff
171302_javaConcept <57761021+zohaib209@users.noreply.github.com>
2020-12-14 14:10:58 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+
+    <title>Zohaib Ejaz | Web Designer, Director | zohaib.ejaz26@gmail.com</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+    <meta name="keywords" content="cv,zohaib, ejaz, zohaibijaz" />
+    <meta name="description" content="" />
+
+    <!--	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" /> -->
+    <link rel="stylesheet" type="text/css" href="resume.css" media="all" />
+
+</head>
+<body>
+
+<div >
+    <div id="inner">
+
+        <div >
+            <div >
+                <div>
+                    <h1>Zohaib Ejaz</h1>
+                    <h2>Web Designer and Android developer</h2>
+                </div>
+
+                <div>
+                    <div>
+                        <h3><a href="zohaib.ejaz26@gmail.com">zohaib.ejaz26@gmail.com</a></h3>
+                        <h3>0323-1458875</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <div>
+
+                    <div>
+                        <div>
+                            <h2>Profile</h2>
+                        </div>
+                        <div >
+                            <p>
+                                Android Developers are mobile technology experts who create applications for mobile devices using the Android platform. Usual work duties of Android Developers are designing new features, collaborating with cross-functional teams, testing code, fixing bugs, and improving application efficiency.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>Skills</h1>
+                        </div>
+                        <div>
+
+                            <div>
+                                <h2>Web Design</h2>
+                                <p>Create creative front-end with css/html.	</p>
+                            </div>
+                            <div>
+                                <h2>Interface Design</h2>
+                                <p>create mobile-app and web-app front-end with xml and html/css respectively.	 </p>
+                            </div>
+                            <div>
+                                <h2>Project Direction</h2>
+                                <p>Proven ability to lead and manage a wide variety of design and development projects in team and independent situations.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>Technical</h1>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+
+                                <li>Android</li>
+                                <li>Java</li>
+                                <li>Kotlin</li>
+
+                                <li>Programming</li>
+                                <li>C++</li>
+                                <li>Java</li>
+                                <li>Data Structure</li>
+                                <li>Algorithm</li>
+                                <li class="last">Linux</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>Experience</h1>
+                        </div>
+
+                        <div>
+                            <div>
+                                <h2>Teknology Solutions</h2>
+                                <h3>Android Developer</h3>
+                                <h4>2019-2020</h4>
+                                <p>Progressively reconceptualize multifunctional "outside the box" thinking through inexpensive methods of empowerment. Compellingly morph extensive niche markets with mission-critical ideas. Phosfluorescently deliver bricks-and-clicks strategic theme areas rather than scalable benefits. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>Education</h1>
+                        </div>
+                        <div >
+                            <h2>PAK-AIMS</h2>
+                            <p>Computer Science <strong>4.0 GPA</strong> </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div class="footer">
+            <p>Zohaib Ejaz <a href="zohaib.ejaz26@gmail.com">zohaib.ejaz26@gmail.com</a> 0323-1458875</p>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+
+
